14ac876bc9117a53e6bfa490b4b48f99464f56aa
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-03-01 17:08:11 +0000

    This is the template for the main web app

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "www.modexabiotech.com",
+  description: "This is the Web app for modexa biotech where all the magic happens",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ Frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center mt-[100px]">
+    <h1 className="text-[40px] animate-bounce duration-100 transition-colors">MODEXA BIOTECH COMING SOON</h1>
+    </div>
+    </>
+  );
+}
