6336e1e0aa84415c5398cd425ae30a2ce25cc6c8
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-03-09 16:39:06 +0000

    Changes to the Donwload links

--- Frontend/app/components/Hero1.tsx
+++ Frontend/app/components/Hero1.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image"
+import Link from "next/link"
 import illustration from "../assets/hero1.png"
 import playstore from "../assets//playstore.svg"
 import appstore from "../assets/6467932_apple_appstore_icon.svg"
+
 
 
 // This is the hero section on the main page
@@ -14,15 +16,17 @@ const Hero1 = () => {
         <h1 className="text lg:text-[60px] lg:mt-[-150px] font-medium ">Instant <span className="text-green">Medical</span> Healthcare Anywhere</h1>
         <div className="download1 mt-[30px] flex flex-col gap-5">
           <h3 className=" text-[18px]">Install the app</h3>
-
           <div className="flex gap-10">
-            <div className="flex gap-5 justify-center rounded-md items-center bg-black-main w-[220px] h-[70px]">
-              <Image alt="appstore" width={40} height={40} src={playstore} />
-              <div className="flex flex-col">
-                <p className="text-white font-light text-[14px]">GET IT ON</p>
-                <p className="text-white font-bold text-[18px]">Google Play</p>
+            <Link href={"https://modexabio.com/"} passHref target="_blank" rel="noreferrer"> 
+              <div className="flex gap-5 justify-center rounded-md items-center bg-black-main w-[220px] h-[70px]">
+                <Image alt="appstore" width={40} height={40} src={playstore} />
+                <div className="flex flex-col">
+                  <p className="text-white font-light text-[14px]">GET IT ON</p>
+                  <p className="text-white font-bold text-[18px]">Google Play</p>
+                </div>
               </div>
-            </div>
+            </Link>
+            <Link href={"https://modexabio.com"} passHref target="_blank" rel="noreferrer">
             <div className="flex gap-5 justify-center rounded-md items-center bg-black-main w-[220px] h-[70px]">
               <Image alt="appstore" width={40} height={40} src={appstore} />
               <div className="flex flex-col">
@@ -30,9 +34,9 @@ const Hero1 = () => {
                 <p className="text-white font-bold text-[18px]">App Store</p>
               </div>
             </div>
-
+            </Link>
+            
           </div>
-
         </div>
       </div>
       <div className="second-section lg:mt-[-50px]">
